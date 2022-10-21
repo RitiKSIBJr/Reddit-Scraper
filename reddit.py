25f4be5662faa175072105fbd11927786211a6de
RitiK.11 <108611021+RitikSibjr@users.noreply.github.com>
2022-10-21 08:50:19 +0545

    Update reddit.py

--- reddit.py
+++ reddit.py
@@ -3,6 +3,8 @@ from time import time
 import praw
 
 def scrape():
+    
+    reddit = praw.Reddit(client_id=my_client_id, client_secret=my_client_secret, user_agent=my_user_agent)
 
     for submission in reddit.subreddit('learnpython').hot(limit=None):
         title =     submission.title
